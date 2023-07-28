9f6187e040fa04bb770eae09a0bb8a0510eb4638
Kshitij Pal <kpal645@gmail.com>
2023-07-29 03:25:50 +0530

    Splitting the dataset for creating training, testing and validation, datasets.

--- Detector.py
+++ Detector.py
@@ -21,3 +21,15 @@ import joblib
 
  # Load the dataset
  dataset = ImageFolder(data_dir, transform=transformations)
+
+# Split the dataset into training, validation, and test sets
+ torch.manual_seed(random_seed)
+ train_size = int(0.7 * len(dataset))
+ val_size = int(0.1 * len(dataset))
+ test_size = len(dataset) - train_size - val_size
+ train_ds, val_ds, test_ds = random_split(dataset, [train_size, val_size, test_size])
+
+ # Create data loaders for training, validation, and test sets
+ train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+ val_dl = DataLoader(val_ds, batch_size=batch_size * 2, num_workers=4, pin_memory=True)
+ test_dl = DataLoader(test_ds, batch_size=batch_size * 2, num_workers=4, pin_memory=True)
